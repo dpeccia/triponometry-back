2d70e182b519851b1fad9072dfad0619acead544
dpeccia <diego.peccia@10pines.com>
2022-08-15 23:37:40 -0300

    i accidentally delete this on a merge

--- src/main/kotlin/utn/triponometry/controllers/TripController.kt
+++ src/main/kotlin/utn/triponometry/controllers/TripController.kt
@@ -6,11 +6,13 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import utn.triponometry.domain.CalculatorInputs
 import utn.triponometry.services.TripService
+import java.lang.RuntimeException
 
 @RestController
 @RequestMapping("/trip")
 class TripController(private val tripService: TripService) {
     @PostMapping("/optimal-route")
+    @CrossOrigin(origins = ["http://localhost:3000"])
     @ApiOperation("Calculates the optimal route for the given calculator inputs and returns an ID")
     fun calculateOptimalRoute(@RequestBody calculatorInputs: CalculatorInputs): ResponseEntity<Any> {
         val optimalRouteId = tripService.calculateOptimalRoute(calculatorInputs)
